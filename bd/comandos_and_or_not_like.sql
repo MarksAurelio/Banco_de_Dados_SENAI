b648ac28d470c87aba2fcda89ed5d2fbeacd8644
Marks Aurelio <marksaurelio@yahoo.com>
2024-11-07 21:41:09 -0300

    comandos and, or, not, like

---
+++ bd/comandos_and_or_not_like.sql
@@ -0,0 +1,25 @@
+use campeonatobrasileiro;
+
+select * from time;
+
+select nome, sigla, estado from time;
+
+select * from time where estado = 'SP';
+
+select * from time where estado = 'SP' or estado = 'RJ';
+
+select * from time where estado = 'RJ' and sigla like 'F%';
+
+select * from estadio;
+
+select * from time where estado = 'RJ' and sigla not like 'F%';
+
+select * from time where sigla <> 'COR';
+
+select * from estadio where capacidade < 60000;
+
+select * from estadio where capacidade < 60000 and capacidade > 50000;
+
+select * from estadio where not (capacidade > 50000 and capacidade < 60000);
+
+select * from estadio where nome like '%Arena%';
