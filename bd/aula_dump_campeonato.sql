9790f279f692bd98dd4b3e0866012d033e2c5548
Marks Aurelio <marksaurelio@yahoo.com>
2024-12-04 21:41:21 -0300

    atualizacao aula dump campeonato

--- bd/aula_dump_campeonato.sql
+++ bd/aula_dump_campeonato.sql
@@ -5,18 +5,63 @@ select * from time;
 select * from partida;
 select * from jogador;
 select * from evento;
-
-select
-*
-from(
+select * from vw_mandante;
+create view vw_mandante as
+ (
 select 
 	sigla, 
-    gol_mandante, 
-    gol_visitante,
+    sum(pontos) as pontos,
+    count(*) qt_partidas,
+    sum(if(pontos = 3, 1, 0)) vitoria,
+    sum(if(pontos = 1, 1, 0)) empate,
+    sum(if(pontos = 0, 1, 0)) derrota,
+    sum(gol_mandante) as gols,
+    sum(gol_visitante) as gols_sofridos,
+    sum(gol_mandante - gol_visitante) as saldo_gols
+from (
+	select
+		sigla,
+        gol_mandante,
+        gol_visitante,
     -- ???? como eu sei quantos pontos o time ganhou em cada partida
-	if (gol_mandante > gol_visitante, 3, 
-    if (gol_mandante = gol_visitante, 1, 0)) as pontos
-from partida as p
-inner join time as t on p.id_mandante = t.id_time
-where gol_mandante is not null and gol_visitante is not null
-) tb;
+		if (gol_mandante > gol_visitante, 3, 
+		if (gol_mandante = gol_visitante, 1, 0)) as pontos
+	from partida as p
+	inner join time as t on p.id_mandante = t.id_time
+	where gol_mandante is not null and gol_visitante is not null
+) as tb
+group by sigla);
+
+select * from estadio;
+select * from time;
+select * from partida;
+select * from jogador;
+select * from evento;
+select * from vw_visitante;
+create view vw_visitante as
+ (
+select 
+	sigla, 
+    sum(pontos) as pontos,
+    count(*) qt_partidas,
+    sum(if(pontos = 3, 1, 0)) vitoria,
+    sum(if(pontos = 1, 1, 0)) empate,
+    sum(if(pontos = 0, 1, 0)) derrota,
+    sum(gol_visitante) as gols,
+    sum(gol_mandante) as gols_sofridos,
+    sum(gol_visitante - gol_mandante) as saldo_gols
+from (
+	select
+		sigla,
+        gol_visitante,
+        gol_mandante,
+    -- ???? como eu sei quantos pontos o time ganhou em cada partida
+		if (gol_visitante > gol_mandante, 3, 
+		if (gol_visitante = gol_mandante, 1, 0)) as pontos
+	from partida as p
+	inner join time as t on p.id_visitante = t.id_time
+	where gol_visitante is not null and gol_mandante is not null
+) as tb
+group by sigla); 
+
+
